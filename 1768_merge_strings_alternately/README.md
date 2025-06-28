565df3ec09b8e37d1af8269206384cc6f51525e0
takakaitani <kaitani1116@gmail.com>
2025-06-28 23:13:54 +0900

    Update solution and README for 1768

--- 1768_merge_strings_alternately/README.md
+++ 1768_merge_strings_alternately/README.md
@@ -1,12 +1,15 @@
 # LeetCode 1768 - Merge Strings Alternately
 
 ## Problem  
-[LeetCode Link](https://leetcode.com/problems/merge-strings-alternately/)
+- [LeetCode Link](https://leetcode.com/problems/merge-strings-alternately/)
 
 ## My Solution
 
-### Code: `solution.php`
+### Code
+- [solution.php](./solution.php)
+- Convert both strings to arrays using str_split(), and loop up to the longer length.
+- Use the null coalescing operator (??) to simplify handling of out-of-bound characters.
 
 ### Time & Space Complexity
-- Time: 
-- Space: 
+- Time  : O(n + m)
+- Space : O(1)
--- 1768_merge_strings_alternately/solution.php
+++ 1768_merge_strings_alternately/solution.php
@@ -5,3 +5,27 @@
  *
  * Solution by Takanori Kaitani
  */
+class Solution {
+
+    /**
+     * Merge two strings alternately.
+     * @param  String $word1
+     * @param  String $word2
+     * @return String $merged
+     */
+    function mergeAlternately($word1, $word2): string
+    {
+        $arr_word1 = str_split($word1);
+        $arr_word2 = str_split($word2);
+        $max_len = max(count($arr_word1), count($arr_word2));
+        $merged = '';
+
+        // Merge characters alternately from both arrays
+        for ($i = 0; $i < $max_len; $i++) {
+            $merged .= $arr_word1[$i] ?? '';
+            $merged .= $arr_word2[$i] ?? '';
+        }
+
+        return $merged;
+    }
+}
