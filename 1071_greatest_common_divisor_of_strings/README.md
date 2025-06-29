3247cfa7db12b19b41a3b419c949d4a886dc3132
takakaitani <kaitani1116@gmail.com>
2025-06-29 11:29:18 +0900

    Update solution and README for 1071

--- 1071_greatest_common_divisor_of_strings/README.md
+++ 1071_greatest_common_divisor_of_strings/README.md
@@ -7,8 +7,9 @@
 
 ### Code
 - [solution.php](./solution.php)
-- 
+- To check whether a common divisor string exists, I concatenate the two strings in both orders and compare them.
+- I calculate the GCD of the string lengths in a separate helper function.
 
 ### Time & Space Complexity
-- Time  : O(n)
-- Space : O(n)
+- Time  : O(n + m)
+- Space : O(1)
--- 1071_greatest_common_divisor_of_strings/solution.php
+++ 1071_greatest_common_divisor_of_strings/solution.php
@@ -5,3 +5,29 @@
  *
  * Solution by Takanori Kaitani
  */
+class Solution {
+
+    /**
+     * Find the greatest common divisor of two strings.
+     */
+    function gcdOfStrings(string $str1, string $str2): string
+    {
+        // No common divisor string.
+        if ($str1 . $str2 !== $str2 . $str1) {
+            return '';
+        }
+
+        $gcdLength = $this->gcd(strlen($str1), strlen($str2));
+
+        // Return substring of that gcd length from either string
+        return substr($str1, 0, $gcdLength);
+    }
+
+    /**
+     * Calculate GCD of two numbers
+     */
+    private function gcd(int $a, int $b): int
+    {
+        return $b === 0 ? $a : $this->gcd($b, $a % $b);
+    }
+}
