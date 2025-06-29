f304ade72d3e23f1d87cc59d4901759fbb7b2412
takakaitani <kaitani1116@gmail.com>
2025-06-29 20:47:01 +0900

    Update solution and README for 283

--- 283_move_zeroes/README.md
+++ 283_move_zeroes/README.md
@@ -7,7 +7,10 @@
 
 ### Code
 - [solution.php](./solution.php)
-- 
+- Use the two-pointer technique to overwrite non-zero elements from the beginning.
+- First pointer (`insertPos`) tracks the position to write the next non-zero element.
+- In the first pass, shift all non-zero elements to the front.
+- In the second pass, fill the remaining positions with `0`.
 
 ### Time & Space Complexity
 - Time  : O(n)
--- 283_move_zeroes/solution.php
+++ 283_move_zeroes/solution.php
@@ -5,3 +5,27 @@
  *
  * Solution by Takanori Kaitani
  */
+class Solution {
+
+    /**
+     * Move all zeroes to the end while maintaining the order of non-zero elements.
+     * @param  int[] $nums
+     * @return void
+     */
+    function moveZeroes(array &$nums): void
+    {
+        $insertPos = 0;
+
+        // Move non-zero elements to the front
+        foreach ($nums as $num) {
+            if ($num !== 0) {
+                $nums[$insertPos++] = $num;
+            }
+        }
+
+        // Fill the rest with zeroes
+        while ($insertPos < count($nums)) {
+            $nums[$insertPos++] = 0;
+        }
+    }
+}
