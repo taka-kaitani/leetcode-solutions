23e30e974d8fe57fcc76cd134c6984c78f566a0d
takakaitani <kaitani1116@gmail.com>
2025-06-29 13:31:02 +0900

    Update solution and README for 605

--- 605_can_place_flowers/solution.php
+++ 605_can_place_flowers/solution.php
@@ -5,3 +5,35 @@
  *
  * Solution by Takanori Kaitani
  */
+class Solution {
+
+    /**
+     * Determine if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule.
+     * @param  int[] $flowerbed
+     * @param  int   $n
+     * @return bool
+     */
+    function canPlaceFlowers(array $flowerbed, int $n): bool
+    {
+        $count = 0;
+        $length = count($flowerbed);
+
+        for ($i = 0; $i < $length; $i++) {
+            if (
+                $flowerbed[$i] === 0 &&
+                ($i === 0           || $flowerbed[$i - 1] === 0) &&
+                ($i === $length - 1 || $flowerbed[$i + 1] === 0)
+            ) {
+                // Place a flower here
+                $flowerbed[$i] = 1;
+                $count++;
+            }
+
+            if ($count >= $n) {
+                return true;
+            }
+        }
+
+        return $count >= $n;
+    }
+}
