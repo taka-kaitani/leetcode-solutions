bfd1e70e0113560bfdac5cb863d61f8f0fd2dedb
takakaitani <kaitani1116@gmail.com>
2025-07-02 07:02:20 +0900

    Update solution and README for 643

--- 643_maximum_average_subarray_i/solution.php
+++ 643_maximum_average_subarray_i/solution.php
@@ -5,3 +5,25 @@
  *
  * Solution by Takanori Kaitani
  */
+class Solution {
+
+    /**
+     * Finds the maximum average of a subarray of length k.
+     * @param  int[] $nums
+     * @param  int   $k
+     * @return float
+     */
+    function findMaxAverage(array $nums, int $k): float
+    {
+        $sum = array_sum(array_slice($nums, 0, $k));
+        $max_sum = $sum;
+
+        $count_nums = count($nums);
+        for ($i = $k; $i < $count_nums; $i++) {
+            $sum += $nums[$i] - $nums[$i - $k];
+            $max_sum = max($max_sum, $sum);
+        }
+
+        return $max_sum / $k;
+    }
+}
