b2995ca46f586f409267c318a08b6b4f42dbf92f
takakaitani <kaitani1116@gmail.com>
2025-07-16 21:10:53 +0900

    Update solution and README for 1004

--- 1004_max_consecutive_ones_iii/README.md
+++ 1004_max_consecutive_ones_iii/README.md
@@ -7,7 +7,8 @@
 
 ### Code
 - [solution.php](./solution.php)
-- 
+- We use a sliding window approach to track the number of 0s.
+- When the number of flipped 0s exceeds `k`, move the `left` pointer to the position after the earliest flipped 0.
 
 ### Time & Space Complexity
 - Time  : O(n)
--- 1004_max_consecutive_ones_iii/solution.php
+++ 1004_max_consecutive_ones_iii/solution.php
@@ -5,3 +5,37 @@
  *
  * Solution by Takanori Kaitani
  */
+class Solution {
+
+    /**
+     * Find the maximum length of a contiguous subarray with at most k zeros.
+     * @param int[] $nums
+     * @param int   $k
+     * @return int
+     */
+    function longestOnes(array $nums, int $k): int
+    {
+        $n = count($nums);
+        $left = 0;
+        $zeroCount = 0;
+        $maxLen = 0;
+
+        for ($right = 0; $right < $n; $right++) {
+            if ($nums[$right] === 0) {
+                $zeroCount++;
+            }
+
+            // If we have more than k zeros, move the left pointer
+            while ($zeroCount > $k) {
+                if ($nums[$left] === 0) {
+                    $zeroCount--;
+                }
+                $left++;
+            }
+
+            $maxLen = max($maxLen, $right - $left + 1);
+        }
+
+        return $maxLen;
+    }
+}
