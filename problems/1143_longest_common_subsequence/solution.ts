7ac77c541a224c0b48896a4b710d30a033a2dd28
takakaitani <kaitani1116@gmail.com>
2025-12-06 19:06:41 +0900

    Add solution and README for 1143

--- problems/1143_longest_common_subsequence/solution.ts
+++ problems/1143_longest_common_subsequence/solution.ts
@@ -5,32 +5,40 @@
  * Solution by Takanori Kaitani
  */
 function longestCommonSubsequence(text1: string, text2: string): number {
-    let dp = [...Array(text1.length + 1)].map(() => Array(text2.length + 1).fill(0));
+    const m = text1.length;
+    const n = text2.length;
+    const dp: number[][] = Array.from({ length: m + 1 }, () => new Array(n + 1).fill(0));
 
-    for (let i = 0; i < text1.length; i++) {
-        const ch1 = text1[i];
-        for (let j = 0; j < text2.length; j++) {
-            const ch2 = text2[j];
-            if (ch1 === ch2) dp[i + 1][j + 1] =  dp[i][j] + 1;
-            else dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (text1[i] === text2[j]) dp[i + 1][j + 1] = dp[i][j] + 1;
+            else dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
         }
     }
 
-    return dp[text1.length][text2.length];
-};
+    return dp[m][n];
+}
 
 /**
  * # Approach
- * - Use dynamic programming to find the length of  longest common subsequence (LCS)
- *   between `text1` and `text2`.
- * - Maintain a 2D array `dp`, where `dp[i][j]` represents the LSC length
- *   of `text1[0...i-1]` and `text2[0...j-1]`.
- * - Transition;
- *   - If `text1[i-1] === text2[j-1], then `dp[i][j] = dp[i-1][j-1] + 1`.
- *   - Otherwise, `dp[i][j] = max(dp[i][j-1], dp[i-1][j])`.
- * - Finally, return `dp[m][n]` where `m` and `n` are the length of `text1` and `text2`.
+ * - Use dynamic programming (DP) to find the length
+ *   of the longest common subsequence (LCS) of `text1` and `text2`.
+ * 
+ * - Maintain 2D array `dp` where `dp[i][j]` represents
+ *   the length of LCS of `text1[0..i-1]` and `text2[0..j-1]`.
+ * 
+ * - Transition:
+ *   - If `text1[i] === text2[j]`, these characters can be part of the LCS:
+ *     - `dp[i + 1][j + 1] = dp[i][j] + 1`
+ *   - Otherwise, the LCS up to `(i, j)` must come from either:
+ *     - skipping `text1[i]` -> `dp[i][j + 1]`
+ *     - or skipping `text2[j]` -> `dp[i + 1][j]`
+ *     So we take:
+ *     - `dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1])`
+ * 
+ * - After filling the table, `dp[m][n]` is the length of teh LCS of the full strings.
  * 
  * # Complexity
- * - Time:  O(m × n)
+ * - Time: O(m × n)
  * - Space: O(m × n)
  */
