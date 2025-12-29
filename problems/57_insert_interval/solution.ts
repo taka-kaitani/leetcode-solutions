97b1f7e8186cde8d486f18ead3d9ff12bcd262c1
takakaitani <kaitani1116@gmail.com>
2025-12-29 22:54:08 +0900

    Add solution and README for 57

--- problems/57_insert_interval/solution.ts
+++ problems/57_insert_interval/solution.ts
@@ -32,42 +32,6 @@ function insert(intervals: number[][], newInterval: number[]): number[][] {
 }
 
 /**
- * LeetCode Problem: 57. Insert Interval
- * https://leetcode.com/problems/insert-interval/
- *
- * 3-phase sweep (no isInserted flag)
- * Solution by Takanori Kaitani
- */
-function insert(intervals: number[][], newInterval: number[]): number[][] {
-    const res: number[][] = [];
-    let i = 0;
-
-    let [newStart, newEnd] = newInterval;
-
-    // 1) Add all intervals that end before newInterval starts
-    while (i < intervals.length && intervals[i][1] < newStart) {
-        res.push(intervals[i]);
-        i++;
-    }
-
-    // 2) Merge all intervals that overlap with newInterval
-    while (i < intervals.length && intervals[i][0] <= newEnd) {
-        newStart = Math.min(newStart, intervals[i][0]);
-        newEnd = Math.max(newEnd, intervals[i][1]);
-        i++;
-    }
-    res.push([newStart, newEnd]);
-
-    // 3) Add the remaining intervals
-    while (i < intervals.length) {
-        res.push(intervals[i]);
-        i++;
-    }
-
-    return res;
-}
-
-/**
  * # Approach
  * - `intervals` is sorted and non-overlapping.
  * - Sweep in three phases:
